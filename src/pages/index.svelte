5984b9b7215ef2a73dd19040262a3b3ffc669b2e
erickArita <erick.emao@gmail.com>
2021-04-26 19:16:16 -0600

    rutes

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,10 +1,10 @@
 <script>
   import Header from "../components/header/Header.svelte";
-  import Jobs from "../components/jobs/jobs.svelte";
+  import Jobs from "../components/Jobs/Jobs.svelte";
 </script>
 
 <div style="height: 100vh;">
   <Header />
 </div>
 
-  <Jobs />
+<Jobs />
